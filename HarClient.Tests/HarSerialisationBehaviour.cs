3abc461dde87da205156c435648a06834d6d2649
Jeremy@slartybartfast <jeremy.blamey@gmail.com>
2020-06-21 15:38:14 +1000

    Added more tests for content types.

--- HarClient.Tests/HarSerialisationBehaviour.cs
+++ HarClient.Tests/HarSerialisationBehaviour.cs
@@ -3,6 +3,7 @@ using JBlam.HarClient.Tests.Validation;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
 
@@ -39,5 +40,40 @@ namespace JBlam.HarClient.Tests
             Assert.IsNotNull(harRequest.PostData?.Text);
             HarAssert.IsValidRequest(harRequest);
         }
+
+        [TestMethod]
+        public void MultipartFormContentRequestIsValid()
+        {
+            // TODO: resesarch expected behaviour
+            var multipartRequest = new HttpRequestMessage(HttpMethod.Post, "http://example.net")
+            {
+                Content = new MultipartFormDataContent
+                {
+                    { new StringContent("A"), "A", "file" },
+                    { new StringContent("B"), "B" }
+                }
+            };
+            var harRequest = multipartRequest.CreateHarRequest();
+            Assert.IsTrue(harRequest.PostData.Params.Any());
+            HarAssert.IsValidRequest(harRequest);
+        }
+
+        // TODO: research expected behaviour with binary content.
+
+        [TestMethod]
+        public void StringContentResponseIsValid()
+        {
+            var stringResponse = new HttpResponseMessage(System.Net.HttpStatusCode.OK)
+            {
+                Content = new StringContent("Good job")
+            };
+            var harResponse = stringResponse.CreateHarResponse();
+            Assert.IsNotNull(harResponse.Content?.Text);
+            HarAssert.IsValidResponse(harResponse);
+        }
+
+        // TODO: research expected behaviour with binary content.
+
+        // TODO: research how encoding should work in HAR content.
     }
 }
