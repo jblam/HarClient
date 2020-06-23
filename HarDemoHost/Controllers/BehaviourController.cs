7fd9ae6a2bce99970ade738f1a75a856f4eae443
Jeremy@slartybartfast <jeremy.blamey@gmail.com>
2020-06-23 19:08:27 +1000

    Create an API controller that returns something arbitrary

---
+++ HarDemoHost/Controllers/BehaviourController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HarDemoHost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BehaviourController : ControllerBase
+    {
+
+        public IActionResult Get()
+        {
+            return Ok(new { Content = "Content", Collection = new[] { 1, 2 } });
+        }
+    }
+}
