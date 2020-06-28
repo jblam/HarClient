ee443747b18f88840e940a992c09d9462b8522ff
Jeremy@slartybartfast <jeremy.blamey@gmail.com>
2020-06-28 12:22:46 +1000

    Revised evaluation of test outcome at the client

--- HarDemoHost/Scripts/testcase.ts
+++ HarDemoHost/Scripts/testcase.ts
@@ -8,29 +8,41 @@ export interface ITestCaseResults {
 document.cookie = "key=value;path=/api;expires=Thu, 01-Jan-1970 00:00:01 GMT";
 
 export const arr: ReadonlyArray<ITestCase> = [
-    fetchTestCase("OK", "/api/behaviour/content"),
-    fetchTestCase("Redirect", "/api/behaviour/redirect"),
-    fetchTestCase("Get query string", "/api/behaviour/content?q1=a1&q2=a2"),
-    fetchTestCase("Post JSON", "/api/behaviour/accepted", {
-        method: 'POST',
-        body: JSON.stringify({ content: 'yes' }),
-        headers: { 'Content-Type': 'application/json' }
+    fetchTestCase("OK", { input: "/api/behaviour/content" }),
+    fetchTestCase("Redirect", { input: "/api/behaviour/redirect" }),
+    fetchTestCase("Get query string", { input: "/api/behaviour/content?q1=a1&q2=a2" }),
+    fetchTestCase("Post JSON", {
+        input: "/api/behaviour/accepted",
+        init: {
+            method: 'POST',
+            body: JSON.stringify({ content: 'yes' }),
+            headers: { 'Content-Type': 'application/json' }
+        }
     }),
-    fetchTestCase("Accepted with redirect", "/api/behaviour/accepted?location=/api/behaviour/content", { method: 'POST' }),
-    fetchTestCase("Get XML", "/api/behaviour/content", { headers: { Accept: 'application/xml' } }),
+    fetchTestCase("Not found", { input: "/api/behaviour/not-found" }, r => {
+        if (r.status != 404)
+            throw new Error("Expected HTTP status 404, but received " + r.status);
+    }),
+    fetchTestCase("Accepted with redirect", { input: "/api/behaviour/accepted?location=/api/behaviour/content", init: { method: 'POST' } }),
+    fetchTestCase("Get XML", { input: "/api/behaviour/content", init: { headers: { Accept: 'application/xml' } } }),
     {
         name: "GET binary content",
-        run: async () => await (await fetch("/chemistry-dog.jpg")).blob()
+        run: async () => {
+            let response = await fetch("/chemistry-dog.jpg");
+            assertIsSuccess(response);
+            return await (response).blob();
+        }
     },
     {
         name: "POST binary content",
         run: async (results) => {
             let blob = await results["GET binary content"];
-            await fetch("/api/behaviour/accepted", {
+            let response = await fetch("/api/behaviour/accepted", {
                 method: 'POST',
                 headers: { "Content-Type": "image/jpg" },
                 body: blob
             });
+            assertIsSuccess(response);
         }
     },
     {
@@ -42,32 +54,39 @@ export const arr: ReadonlyArray<ITestCase> = [
             let formData = new FormData();
             formData.append("textData", "text");
             formData.append("imageData", blob, "image.jpg");
-            await fetch("/api/behaviour/accepted", {
+            let response = await fetch("/api/behaviour/accepted", {
                 method: 'POST',
                 body: formData
             });
+            assertIsSuccess(response);
         }
     },
-    fetchTestCase("POST multipart form", "/api/behaviour/accepted", {
-        method: 'POST',
-        body: (() => {
-            // produces application/form-multipart
-            let f = new FormData();
-            f.append("x", "x");
-            f.append("y", "a+b+c/d:e?f");
-            return f;
-        })()
+    fetchTestCase("POST multipart form", {
+        input: "/api/behaviour/accepted",
+        init: {
+            method: 'POST',
+            body: (() => {
+                // produces application/form-multipart
+                let f = new FormData();
+                f.append("x", "x");
+                f.append("y", "a+b+c/d:e?f");
+                return f;
+            })()
+        }
     }),
-    fetchTestCase("POST urlencoded", "/api/behaviour/accepted", {
-        method: 'POST',
-        body: (() => {
-            // produces application/x-www-form-urlencoded
-            // equivalent to an actual genuine <form>
-            let p = new URLSearchParams();
-            p.append("x", "x");
-            p.append("y", "a+b+c/d:e?f");
-            return p;
-        })()
+    fetchTestCase("POST urlencoded", {
+        input: "/api/behaviour/accepted",
+        init: {
+            method: 'POST',
+            body: (() => {
+                // produces application/x-www-form-urlencoded
+                // equivalent to an actual genuine <form>
+                let p = new URLSearchParams();
+                p.append("x", "x");
+                p.append("y", "a+b+c/d:e?f");
+                return p;
+            })()
+        }
     }),
     {
         name: "cancelled request",
@@ -79,15 +98,7 @@ export const arr: ReadonlyArray<ITestCase> = [
             return fetchResult;
         }
     },
-    {
-        name: "POST set cookies",
-        run: async () => {
-            var response = await fetch("/api/behaviour/set-cookie", { method: 'POST' });
-            if (!document.cookie) {
-                throw new Error("`document.cookie` is still empty");
-            }
-        }
-    },
+    fetchTestCase("POST set cookies", { input: "/api/behaviour/set-cookie", init: { method: 'POST' } }),
     {
         name: "GET check cookies",
         run: async () => {
@@ -101,9 +112,16 @@ export const arr: ReadonlyArray<ITestCase> = [
     }
 ];
 
-function fetchTestCase(name: string, input: RequestInfo, init?: RequestInit): ITestCase {
+function assertIsSuccess(response: Response) {
+    if (response.status < 200 || response.status > 299)
+        throw new Error("Response status does not indicate success");
+    return response;
+}
+function fetchTestCase(name: string, fetchArgs: { input: RequestInfo, init?: RequestInit }, validation?: (response: Response) => any): ITestCase {
+    let { input, init } = fetchArgs;
     return {
         name,
-        run: () => fetch(input, init)
+        run: () => fetch(input, init).then(validation || assertIsSuccess)
     };
 }
+
